38f9e022bba7d0975066b6f7f5f1cbef90a0f3ba
Alexander Martinz <alexander@ubports.com>
2022-06-29 17:34:18 +0200

    plugins: add new gestures plugin
    Add a new gestures plugin, which will be shown in system settings
    in the "Personal" category.
    
    Right now it only supports Double Tap To Wake (aka DT2W) and hides
    itself if DT2W is not supported.
    
    Change-Id: I49f9a76541dc620a77650e7c0dcd6175df8fe1f4
    Signed-off-by: Alexander Martinz <alexander@ubports.com>

---
+++ plugins/gestures/plugin/gestures-plugin.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2022 UBports Foundation
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <QObject>
+#include <LomiriSystemSettings/PluginInterface>
+
+class GesturesPlugin: public QObject, public LomiriSystemSettings::PluginInterface2
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "com.ubuntu.SystemSettings.PluginInterface/2.0")
+    Q_INTERFACES(LomiriSystemSettings::PluginInterface2)
+
+public:
+    LomiriSystemSettings::ItemBase *createItem(const QVariantMap &staticData,
+                                         QObject *parent = 0);
+};
+
