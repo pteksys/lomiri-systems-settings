7826190d8444c1299e53ee431b4fea781e5855b4
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2022-10-05 17:29:50 +0200

    copyright headers: Correctly spell 'Foundation' (with capital 'F').

--- plugins/gestures/plugin.cpp
+++ plugins/gestures/plugin.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 UBports foundation
+ * Copyright (C) 2022 UBports Foundation
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3 as
