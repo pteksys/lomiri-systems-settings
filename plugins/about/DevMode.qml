pteksys/lomiri-systems-settings

commit 02667dfaaa8fe74bba5822316d80dfe7afc05601

Pass instance of LomiriSecurityPrivacyPanel to LockSecurity page
This is necessary after the changes in
lomiri-system-settings-security-privacy!7.

plugins/about/DevMode.qml
@@ -112,7 +112,7 @@ ItemPage {
                 objectName: "lockSecurityItem"
                 text: i18n.tr("Lock security")
                 onClicked: pageStack.addPageToNextColumn(
-                    devModePage, Qt.resolvedUrl("../security-privacy/LockSecurity.qml")
+                    devModePage, Qt.resolvedUrl("../security-privacy/LockSecurity.qml", {securityPrivacy: securityPrivacy})
                 )
             }
         }

commit 320f3775da95d978b903fbe9f87021aac201cfce

plugins/about: fix bracket mistake in DevMode page
The parameters of `Qt.resolvedUrl()` isn't supposed to include
`{securityPrivacy...` thing. So, move the bracket into the right place.
While we're at it, also format the code so that it looks better and is
easier to spot a similar kind of mistake.

I can't believe I let this slip in. Maybe I should look for a new pair
of glasses. 🤦

Fixes: 02667dfa ("Pass instance of LomiriSecurityPrivacyPanel to
LockSecurity page")

plugins/about/DevMode.qml
@@ -112,7 +112,9 @@ ItemPage {
                 objectName: "lockSecurityItem"
                 text: i18n.tr("Lock security")
                 onClicked: pageStack.addPageToNextColumn(
-                    devModePage, Qt.resolvedUrl("../security-privacy/LockSecurity.qml", {securityPrivacy: securityPrivacy})
+                    devModePage,
+                    Qt.resolvedUrl("../security-privacy/LockSecurity.qml"),
+                    {securityPrivacy: securityPrivacy}
                 )
             }
         }