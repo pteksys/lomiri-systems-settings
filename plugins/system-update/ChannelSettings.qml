ubports/system-settings

commit 9e14f23ca46963c6be07f959fe31468a74817e61

[system-image] Improve channelSettings
This improves channel switching

- Cancel current update before switching
- Fix not selecting current channel
- Give better user info

plugins/system-update/ChannelSettings.qml
@@ -47,7 +47,6 @@ ItemPage {
         if (channelSelectorModel.count !== 0)
           return;
         appendChannels()
-        setSelectedChannel()
         return;
       }
     }
@@ -66,22 +65,43 @@ ItemPage {
           var prettyChannel = prettyChannels[channel[channel.length-1]] ? prettyChannels[channel[channel.length-1]] : channel[channel.length-1];
           channelSelectorModel.append({ name: prettyChannel, description: "", channel: _channel});
       });
+      setSelectedChannel();
     }
 
     function setSelectedChannel() {
-      var channel = SystemImage.getSwitchChannel().split("/");
-      channelSelector.selectedIndex = channel[channel.length-1];
-      return channel[channel.length-1];
+      var selNum = 0;
+      var channel = SystemImage.getSwitchChannel();
+      for (var i = 0; i <= channelSelectorModel.count; i++) {
+        if (channelSelectorModel.get(i).channel === channel){
+            selNum = i;
+            break;
+        }
+      }
+      channelSelector.selectedIndex = selNum;
+      return selNum;
     }
 
-    ActivityIndicator {
-      id: aIndicator
-      opacity: visible ? 1 : 0
-      visible: false
-      running: visible
-      anchors.horizontalCenter: parent.horizontalCenter
-      anchors.verticalCenter: parent.verticalCenter
+    Column {
+        id: aIndicator
+        anchors.horizontalCenter: parent.horizontalCenter
+        anchors.verticalCenter: parent.verticalCenter
+        visible: false
+
+        ActivityIndicator {
+            opacity: visible ? 1 : 0
+            visible: parent.visible
+            running: visible
+            anchors.horizontalCenter: parent.horizontalCenter
+        }
+
+        Label {
+            id: aIndicatorText
+            text: i18n.tr("Fetching channels")
+            visible: parent.visible
+        }
     }
+
+
 
     ListItem.ItemSelector {
         id: channelSelector
@@ -93,12 +113,16 @@ ItemPage {
           setSelectedChannel()
         }
         onSelectedIndexChanged: {
+          // Cancel all ongoing updates before switching
+          SystemImage.cancelUpdate()
           SystemImage.setSwitchChannel(channelSelectorModel.get(selectedIndex).channel);
-          SystemImage.setSwitchBuild(0);
+
+          if (channelSelectorModel.get(selectedIndex).channel === SystemImage.channelName)
+            SystemImage.setSwitchBuild(SystemImage.currentBuildNumber);
+          else
+            SystemImage.setSwitchBuild(0);
           UpdateManager.check(UpdateManager.CheckImage);
-        }
-        Component.onCompleted: {
-          setSelectedChannel()
+          aIndicatorText.text = i18n.tr("Switching channel");
         }
     }
 
@@ -110,7 +134,6 @@ ItemPage {
     ListModel {
         id: channelSelectorModel
         Component.onCompleted: {
-          console.error(UpdateManager)
           switch (UpdateManager.status) {
           case UpdateManager.StatusCheckingImageUpdates:
           case UpdateManager.StatusCheckingAllUpdates:
@@ -120,7 +143,6 @@ ItemPage {
           }
           if (SystemImage.getChannels().length !== 0){
             appendChannels()
-            setSelectedChannel()
             return;
           }
           UpdateManager.check(UpdateManager.CheckImage);

pteksys/lomiri-systems-settings

commit e923f1eabcd42bd9eaa91ae56593dbe3e539ab05

system-update: fix channel selector UI stuck on the first entry
Fix an off-by-one error introduced in 9e14f23ca469 ("[system-image]
Improve channelSettings"), which causes the UI to always show the first
channel as selected.

And while we're at it, initialize the default selection to -1 instead of
0 to make sure we don't show any channel selected when e.g. we're on a
hidden channel.

(cherry picked from commit a763e753a333e5a74a1a3775f5f2eb43d6ece97b)

plugins/system-update/ChannelSettings.qml
@@ -76,9 +76,9 @@ ItemPage {
     }
 
     function setSelectedChannel() {
-      var selNum = 0;
+      var selNum = -1;
       var channel = SystemImage.getSwitchChannel();
-      for (var i = 0; i <= channelSelectorModel.count; i++) {
+      for (var i = 0; i < channelSelectorModel.count; i++) {
         if (channelSelectorModel.get(i).channel === channel){
             selNum = i;
             break;